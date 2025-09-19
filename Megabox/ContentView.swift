6dcaf59f8a78726e8016535925c4f351acd7d553
엄지용 <eom175@naver.com>
2025-09-19 09:57:16 +0900

    Initial Commit

---
+++ Megabox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Megabox
+//
+//  Created by 엄지용 on 9/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MegaboxTests/MegaboxTests.swift
@@ -0,0 +1,17 @@
+//
+//  MegaboxTests.swift
+//  MegaboxTests
+//
+//  Created by 엄지용 on 9/19/25.
+//
+
+import Testing
+@testable import Megabox
+
+struct MegaboxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
